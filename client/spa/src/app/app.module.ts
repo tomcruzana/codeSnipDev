868d03a570bc371d2f658be9f05cdd252f8b6b13
tomcruzana <tomcruzana@ymail.com>
2022-09-17 17:36:36 -0700

    added main jumbotron

--- client/spa/src/app/app.module.ts
+++ client/spa/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule
