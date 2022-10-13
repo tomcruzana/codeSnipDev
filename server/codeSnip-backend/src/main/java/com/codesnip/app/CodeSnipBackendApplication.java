454713a06c58518856532a53bf66c6d8a5f43174
tomcruzana <tomcruzana@ymail.com>
2022-10-12 19:41:52 -0700

    reinitialized new version of spring boot project

---
+++ server/codeSnip-backend/src/main/java/com/codesnip/app/CodeSnipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codesnip.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnipBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSnipBackendApplication.class, args);
+	}
+
+}
