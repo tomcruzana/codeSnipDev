8ed3b9d92b042b2f4aa4e0e6a04b29a61fbbb80e
tomcruzana <tomcruzana@ymail.com>
2022-10-13 20:31:44 -0700

    added csrf token protection

--- server/codeSnip-backend/src/main/java/com/codesnip/app/util/SecurityConfig.java
+++ server/codeSnip-backend/src/main/java/com/codesnip/app/util/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -21,6 +22,7 @@ public class SecurityConfig {
 		// CORS configuration
 		// route security configurations
 		http.cors().configurationSource(new CorsConfigurationSource() {
+
 			@Override
 			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 				CorsConfiguration config = new CorsConfiguration();
@@ -31,8 +33,13 @@ public class SecurityConfig {
 				config.setMaxAge(3600L);
 				return config;
 			}
-		}).and().csrf().disable().authorizeRequests().antMatchers("/login", "/dashboard", "/profile").authenticated()
-				.antMatchers("/home", "/documentation", "/pricing", "/register", "/share").permitAll().and().formLogin()
+		}).and().csrf().ignoringAntMatchers("/register")
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+				.and()
+				.authorizeRequests()
+				.antMatchers("/login", "/dashboard", "/profile").authenticated()
+				.antMatchers("/home", "/documentation", "/pricing", "/register", "/share").permitAll()
+				.and().formLogin()
 				.and().httpBasic();
 		return http.build();
 	}
