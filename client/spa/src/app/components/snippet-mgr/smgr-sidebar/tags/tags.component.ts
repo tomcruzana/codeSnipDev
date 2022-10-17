c43247d103d9761da497149d0dc3375b0eb4323f
tomcruzana <tomcruzana@ymail.com>
2022-10-17 05:40:21 -0700

    added new tag form

--- client/spa/src/app/components/snippet-mgr/smgr-sidebar/tags/tags.component.ts
+++ client/spa/src/app/components/snippet-mgr/smgr-sidebar/tags/tags.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
-  styleUrls: ['./tags.component.css']
+  styleUrls: ['./tags.component.css'],
 })
 export class TagsComponent implements OnInit {
+  // array varaible that holds the tags from the db
+  tags = new Array<string>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.tags.push('asdasd');
+    this.tags.push('asdasd');
+    this.tags.push('asdasd');
+    this.tags.push('asdasd');
+    this.tags.push('asdasd');
+    this.tags.push('asdasd');
+    this.tags.push('asdasd');
   }
-
 }
