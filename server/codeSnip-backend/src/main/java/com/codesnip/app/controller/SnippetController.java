86d25b9ef41afbea08bfe60c12e3d7921b86ab1e
tomcruzana <tomcruzana@ymail.com>
2022-10-19 13:32:31 -0700

    updated update snippet code endpoint

--- server/codeSnip-backend/src/main/java/com/codesnip/app/controller/SnippetController.java
+++ server/codeSnip-backend/src/main/java/com/codesnip/app/controller/SnippetController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,8 +48,8 @@ public class SnippetController {
 		return new ResponseEntity<>(updateSuccessMessage, HttpStatus.OK);
 	}
 
-	@PatchMapping("/snippet/save")
-	public ResponseEntity<String> updateSnippetCode(@RequestParam int id, @RequestParam(name = "code") String code)
+	@PatchMapping("/snippet/save/{id}")
+	public ResponseEntity<String> updateSnippetCode(@PathVariable int id, @RequestBody() String code)
 			throws Exception {
 		// update entity
 		snippetService.saveById(id, code);
